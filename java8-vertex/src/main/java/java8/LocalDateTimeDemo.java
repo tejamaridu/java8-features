feff8b8d7951e8f0aa917604c58073224b480c2d
Teja Mr <teja.maridu@gmail.com>
2023-07-13 19:40:52 -0400

    Java 8 Date methods added

--- java8-vertex/src/main/java/java8/LocalDateTimeDemo.java
+++ java8-vertex/src/main/java/java8/LocalDateTimeDemo.java
@@ -3,6 +3,10 @@ package java8;
 import java.time.*;
 import java.time.format.DateTimeFormatter;
 import java.time.temporal.TemporalAdjusters;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
 
 public class LocalDateTimeDemo {
 
@@ -197,5 +201,36 @@ public class LocalDateTimeDemo {
         System.out.println("************************************");
 
 
+        // ********* Java Date API Legacy Date Time Support *********
+
+        {
+            //Date to Instant
+            Instant timestamp = new Date().toInstant();
+            //Now we can convert Instant to LocalDateTime or other similar classes
+            LocalDateTime date = LocalDateTime.ofInstant(timestamp,
+                    ZoneId.of(ZoneId.SHORT_IDS.get("PST")));
+            System.out.println("Date = " + date);
+
+            //Calendar to Instant
+            Instant time = Calendar.getInstance().toInstant();
+            System.out.println(time);
+            //TimeZone to ZoneId
+            ZoneId defaultZone = TimeZone.getDefault().toZoneId();
+            System.out.println(defaultZone);
+
+            //ZonedDateTime from specific Calendar
+            ZonedDateTime gregorianCalendarDateTime = new GregorianCalendar().toZonedDateTime();
+            System.out.println(gregorianCalendarDateTime);
+
+            //Date API to Legacy classes
+            Date dt = Date.from(Instant.now());
+            System.out.println(dt);
+
+            TimeZone tz = TimeZone.getTimeZone(defaultZone);
+            System.out.println(tz);
+
+            GregorianCalendar gc = GregorianCalendar.from(gregorianCalendarDateTime);
+            System.out.println(gc);
+        }
     }
 }
